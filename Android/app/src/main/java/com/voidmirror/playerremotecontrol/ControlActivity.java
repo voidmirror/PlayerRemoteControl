50420b1e14de21f2616847a2ebde6d6ba44b023d
voidmirror <49756045+voidmirror@users.noreply.github.com>
2022-08-22 10:51:30 +0300

    Topbar menu fixed

--- Android/app/src/main/java/com/voidmirror/playerremotecontrol/ControlActivity.java
+++ Android/app/src/main/java/com/voidmirror/playerremotecontrol/ControlActivity.java
@@ -6,12 +6,14 @@ import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.NumberPicker;
+import android.widget.PopupMenu;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -28,8 +30,9 @@ public class ControlActivity extends Activity {
     Button btnFullscreen;
     Button btnSoundUp;
     Button btnSoundDown;
+    Button btnMenu;
 
-    Spinner dropdown;
+//    Spinner dropdown;
 
     HttpController httpController;
 
@@ -41,43 +44,43 @@ public class ControlActivity extends Activity {
         httpController = HttpController.getInstance();
 //        LastResponseObserver lastResponseObserver = new LastResponseObserver(this, httpController.getLastResponse());
 
-        dropdown = findViewById(R.id.dropdown);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.controllerDropdown, android.R.layout.simple_spinner_dropdown_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown.setAdapter(adapter);
-        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        //TODO: openning new video in browser
-                        System.out.println("There will be new video soon");
-                        break;
-                    case 1:
-                        //TODO: send signal to shutdown a computer
-                        System.out.println(("There a computer will be shutingdown"));
-
-                        break;
-                    case 2:
-                        httpController.sendSignal(httpController.makeRequest(RequestType.TIMER, String.valueOf(-1)));
-//                        httpController.getLastResponse()
-//                                .observeOn(AndroidSchedulers.mainThread())
-//                                .subscribeOn(Schedulers.io())
-//                                .subscribe(v -> {
-//                                    System.out.println("### Subscribing on LastResponse");
-//                                    Toast toast = Toast.makeText(ControlActivity.this, v, Toast.LENGTH_SHORT);
-//                                    toast.show();
-//                                    httpController.recreateLastResponse();
-//                                });
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+//        dropdown = findViewById(R.id.dropdown);
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.controllerDropdown, android.R.layout.simple_spinner_dropdown_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        dropdown.setAdapter(adapter);
+//        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                switch (position) {
+//                    case 0:
+//                        //TODO: openning new video in browser
+//                        System.out.println("There will be new video soon");
+//                        break;
+//                    case 1:
+//                        //TODO: send signal to shutdown a computer
+//                        System.out.println(("There a computer will be shutingdown"));
+//
+//                        break;
+//                    case 2:
+//                        httpController.sendSignal(httpController.makeRequest(RequestType.TIMER, String.valueOf(-1)));
+////                        httpController.getLastResponse()
+////                                .observeOn(AndroidSchedulers.mainThread())
+////                                .subscribeOn(Schedulers.io())
+////                                .subscribe(v -> {
+////                                    System.out.println("### Subscribing on LastResponse");
+////                                    Toast toast = Toast.makeText(ControlActivity.this, v, Toast.LENGTH_SHORT);
+////                                    toast.show();
+////                                    httpController.recreateLastResponse();
+////                                });
+//                        break;
+//                }
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
         btnShiftLeft = findViewById(R.id.btnShiftLeft);
         btnShiftRight = findViewById(R.id.btnShiftRight);
@@ -86,6 +89,7 @@ public class ControlActivity extends Activity {
         btnFullscreen = findViewById(R.id.btnFullscreen);
         btnSoundUp = findViewById(R.id.soundUp);
         btnSoundDown = findViewById(R.id.soundDown);
+        btnMenu = findViewById(R.id.btnMenu);
 
         btnShiftLeft.setOnClickListener(view -> {
             httpController.sendSignal(httpController.makeRequest(RequestType.CODE, "shiftLeft"));
@@ -105,8 +109,8 @@ public class ControlActivity extends Activity {
         btnSoundDown.setOnClickListener(view -> {
             httpController.sendSignal(httpController.makeRequest(RequestType.CODE, "soundDown"));
         });
+        btnMenu.setOnClickListener(this::openPopupMenu);
         btnTimer.setOnClickListener(view -> {
-
 
             NumberPicker numberPicker = new NumberPicker(ControlActivity.this);
             numberPicker.setMinValue(1);
@@ -152,6 +156,27 @@ public class ControlActivity extends Activity {
 //                    httpController.recreateLastResponse();
 //                });
 
+    }
+
+    private void openPopupMenu(View view) {
+        PopupMenu menu = new PopupMenu(this, view);
+        menu.inflate(R.menu.popupmenu);
+        menu.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menuNewVideo:
+                    return true;
+                case R.id.menuImmediateShutdown:
+                    return true;
+                case R.id.menuCancelShutdown:
+                    httpController.sendSignal(httpController.makeRequest(RequestType.TIMER, String.valueOf(-1)));
+                    return true;
+                default:
+                    return false;
+            }
+        });
+//        menu.setOnDismissListener(menu1 -> Toast.makeText(ControlActivity.this, "onDismiss",
+//                Toast.LENGTH_SHORT).show());
+        menu.show();
     }
 
 }
